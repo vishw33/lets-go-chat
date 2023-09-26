8340ef68d772247801aec1fa6573f711957af46c
vishw33 <vishw33@yahoo.com>
2023-09-26 18:24:09 +0530

    Documenting in go

--- pkg/hasher.go
+++ pkg/hasher.go
@@ -1,9 +1,14 @@
+// Package hasher package is used to demonstrate the understanding of
+// how packages work in go
 package hasher
 
+// HashPassword return the decrypted password if able to decrypt
+// else return error
 func HashPassword(password string) (string, error) {
 	return "hello", nil
 }
 
+// CheckPasswordHash return bool based on condition
 func CheckPasswordHash(password, hash string) bool {
 	return true
 }
